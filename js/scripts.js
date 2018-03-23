0c191c41d8aa4a0c756e85332eb91997149804b0
Bobby Martin <robert.martin.619@gmail.com>
2018-03-23 10:54:25 -0700

    Add first conditional

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,15 @@ $(document).ready(function() {
         var questionFour = parseInt($("#qFour").val());
         var questionFive = parseInt($("#qFive").val());
         var result = questionOne + questionTwo + questionThree + questionFour + questionFive;
-        // if(result <= 4) {
-        //
-        // } else if(result >= 7) {
-        //
-        // } else if(result > 8) {
-        //
-        // } else {
-        //
-        // }
+        if(result <= 4) {
+          $("#frontEnd").show();
+        } else if(result >= 7) {
+
+        } else if(result > 8) {
+
+        } else {
+
+        }
         event.preventDefault();
     });
 });
