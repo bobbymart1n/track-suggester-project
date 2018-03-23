a7eb60e4c320395c4f0aa2a221c45c371ee9c774
Bobby Martin <robert.martin.619@gmail.com>
2018-03-23 13:45:45 -0700

    Add name entry and logic to display the name user puts in

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,40 @@
 $(document).ready(function() {
-    $("#formOne").submit(function(event) {
-        var questionOne = parseInt($("#qOne").val());
-        var questionTwo = parseInt($("#qTwo").val());
-        var questionThree = parseInt($("#qThree").val());
-        var questionFour = parseInt($("#qFour").val());
-        var questionFive = parseInt($("#qFive").val());
-        var result = questionOne + questionTwo + questionThree + questionFour + questionFive;
-        if(result === 4) {
-          $("#frontEnd").show();
-        } else if(result === 7) {
-          $("#backEnd").show();
-        } else if(result > 7) {
-          $("#java").show();
-        } else if(result < 0) {
-          $("#maybeNot").show();
-        }  else {
-          $("#any").show();
-        }
-        // Hide additional survey results if survey is retaken.
-        $("button.btn").click(function() {
-          $(".survey-card").hide();
-          $(".survey-result").show();
-        });
-        // Hide survey questions on form submit
-        $(".survey").hide();
-        // When result shows, show a close button in top right corner of result card
-        $(".back-button").show();
-        // On close button click, show survey again and hide results
-        $(".back-button").click(function() {
-          $(".survey").show();
-          // Reset form back to their first options/zeroeth positions
-          $("#formOne")[0].reset();
-          $(".survey-result").hide();
-        });
-        event.preventDefault();
+  $("#formOne").submit(function(event) {
+    var name = $("#name").val();
+    var questionOne = parseInt($("#qOne").val());
+    var questionTwo = parseInt($("#qTwo").val());
+    var questionThree = parseInt($("#qThree").val());
+    var questionFour = parseInt($("#qFour").val());
+    var questionFive = parseInt($("#qFive").val());
+    var result = questionOne + questionTwo + questionThree + questionFour + questionFive;
+    if(result === 4) {
+      $("#frontEnd").show();
+    } else if(result === 7) {
+      $("#backEnd").show();
+    } else if(result > 7) {
+      $("#java").show();
+    } else if(result < 0) {
+      $("#maybeNot").show();
+    }  else {
+      $("#any").show();
+    }
+    $(".user-name").text(name);
+    // Hide additional survey results if survey is retaken.
+    $("button.btn").click(function() {
+      $(".survey-card").hide();
+      $(".survey-result").show();
     });
+    // Hide survey questions on form submit
+    $(".survey").hide();
+    // When result shows, show a close button in top right corner of result card
+    $(".back-button").show();
+    // On close button click, show survey again and hide results
+    $(".back-button").click(function() {
+      $(".survey").show();
+      // Reset form back to their first options/zeroeth positions
+      $("#formOne")[0].reset();
+      $(".survey-result").hide();
+    });
+    event.preventDefault();
+  });
 });
