6ac92c58993c3492036cf1ae5e790707034cde64
Bobby Martin <robert.martin.619@gmail.com>
2018-03-23 10:20:06 -0700

    Fix tab length, and add variables to questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     $("#formOne").submit(function(event) {
         var questionOne = $("#qOne").val();
+        var questionTwo = $("#qTwo").val();
+        var questionThree = $("#qThree").val();
+        var questionFour = $("#qFour").val();
+        var questionFive = $("#qFive").val();
+
         
         event.preventDefault();
     });
