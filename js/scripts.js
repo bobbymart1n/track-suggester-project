6007faa5dea49b8aed9c21990735aa6f8d6929e4
Bobby Martin <robert.martin.619@gmail.com>
2018-03-23 10:31:55 -0700

    Add result to jQuery, change music values

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     $("#formOne").submit(function(event) {
-        var questionOne = $("#qOne").val();
-        var questionTwo = $("#qTwo").val();
-        var questionThree = $("#qThree").val();
-        var questionFour = $("#qFour").val();
-        var questionFive = $("#qFive").val();
+        var questionOne = parseInt($("#qOne").val());
+        var questionTwo = parseInt($("#qTwo").val());
+        var questionThree = parseInt($("#qThree").val());
+        var questionFour = parseInt($("#qFour").val());
+        var questionFive = parseInt($("#qFive").val());
+        var result = questionOne + questionTwo + questionThree + questionFour + questionFive;
+        console.log(result);
+        if(result ) {
 
-        
+        }
         event.preventDefault();
     });
 });
